99fef21625514e211077ab69c29e6340d6237a50
Handballfreak <56076969+Handballfreak@users.noreply.github.com>
2019-12-02 17:26:05 +0100

    Testfile

--- ScreenshotTool.py
+++ ScreenshotTool.py
@@ -1,5 +1,12 @@
-#Test
-import matplotlib.pyplot as plt
-print("Hello")
-print("Noob")
-print("test")
+import pyautogui
+import time
+# Sleep for 5 seconds to allow me to open book
+time.sleep(5)
+# Range can be changed depending on the number of pages
+for i in range(1):
+ # Turn page
+ pyautogui.keyDown('right')
+ pyautogui.keyUp('right')
+ # Take and save a screenshot
+ pyautogui.screenshot('images/page_%d.pdf' % i)
+ time.sleep(0.05)
