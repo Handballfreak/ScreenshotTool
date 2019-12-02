e33fd7d6d9215d9eef5424b59431cd8e282315a3
Handballfreak <56076969+Handballfreak@users.noreply.github.com>
2019-12-02 17:30:09 +0100

    Update ScreenshotTool.py
    plac

--- ScreenshotTool.py
+++ ScreenshotTool.py
@@ -1,12 +1,14 @@
 import pyautogui
 import time
-# Sleep for 5 seconds to allow me to open book
+
+# Sleep for 5 seconds
 time.sleep(5)
+
 # Range can be changed depending on the number of pages
 for i in range(1):
- # Turn page
  pyautogui.keyDown('right')
  pyautogui.keyUp('right')
+ 
  # Take and save a screenshot
  pyautogui.screenshot('images/page_%d.pdf' % i)
  time.sleep(0.05)
